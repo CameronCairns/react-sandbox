7589147d15b6bf0f4ab5b68743d00e437837083f
Cameron Cairns <code@cameroncairns.com>
2018-12-06 08:17:51 -0800

    Adds some controlled components
    Side note, multiselect is not intuitive. It breaks react's "everything
    is just a value" idea to hae to reference the html nodes directly.
    Stumbling block was that event.target.value for multiselect just tells
    you what the user recently clicked, not what the user's entire selection
    is.  I can see that being useful, but it seems secondary to finding all
    the values selected by the user in my opinion.

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function RedListItem({ value }) {
 function TestNumberList({ numbers }) {
   const listItems = numbers.map(number => (
     // Note that the key is defined here, keys must be defined
-    // in jsx that is directly nested within the ul/ol
+    // in jsx that is directly nested within the parent container
     <RedListItem value={number} key={number.toString()} />
   ));
   return <ul>{listItems}</ul>;
@@ -80,11 +80,146 @@ function TestForms() {
     "week"
   ];
   const inputs = inputTypes.map(inputType => (
-    <div>
+    <div key={inputType}>
       <input type={inputType} />
     </div>
   ));
   return <form>{inputs}</form>;
+}
+
+class ControlledForm extends React.Component<{}, { value: string }> {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.value}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+function CustomTextArea() {
+  const text = `
+    Let's try fancy multiline strings
+    ${50} with a variable here. Apparently
+    template strings respect whitespace..
+    It makes sense but hurts splits of 80 chars.
+    However there is a tag system that might be
+    utilized to get around this.
+  `;
+  return <textarea value={text} />;
+}
+
+class IceCreamSelector extends React.Component<{}, { value: string }> {
+  constructor(props) {
+    super(props);
+    this.state = { value: "strawberry" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(`Two scoops of ${this.state.value} coming up!`);
+    event.preventDefault();
+  }
+
+  render() {
+    const flavors = ["Strawberry", "Chocolate", "Vanilla", "Boysenberry"];
+    const options = flavors.map(flavor => (
+      <option key={flavor.toLowerCase()}>{flavor}</option>
+    ));
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          What type of icecream would you like?
+          <select value={this.state.value} onChange={this.handleChange}>
+            {options}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class TestMultiSelector extends React.Component<{}, { value: [] }> {
+  state = { value: ["Larry", "Moe"] };
+
+  handleChange = event => {
+    const selectedOptions = Array.prototype.filter.call(
+      event.target.options,
+      option => option.selected
+    );
+    const value = selectedOptions.reduce(
+      (acc, option) => acc.concat([option.value]),
+      []
+    );
+    this.setState({ value: value });
+  };
+
+  handleSubmit = event => {
+    var message;
+    if (this.state.value.length === 0) {
+      message = "You gotta choose at least one stooge!";
+    } else {
+      function reducer(acc, val) {
+        return `${acc}, ${val}`;
+      }
+      const selections = this.state.value.reduce(reducer);
+      message = `The following was selected:\n ${selections}`;
+    }
+    alert(message);
+    event.preventDefault();
+  };
+
+  render() {
+    const choices = ["Larry", "Moe", "Curley"];
+    const options = choices.map(choice => (
+      <option key={choice.toLowerCase()}>{choice}</option>
+    ));
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Choose a stooge (1 or more):
+          <select
+            multiple
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
+            {options}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 class App extends React.Component<{}, { showTime: boolean }> {
@@ -125,8 +260,12 @@ class App extends React.Component<{}, { showTime: boolean }> {
           <Clock />
         </h2>
         <TestNumberList numbers={[1, 2, 3, 4, 5]} />
-        <NumberList rangeStart={0} rangeStop={100} />
+        <NumberList rangeStart={0} rangeStop={10} />
         <TestForms />
+        <ControlledForm />
+        <CustomTextArea />
+        <IceCreamSelector />
+        <TestMultiSelector />
       </div>
     );
   }
